4039e3f2f007213f733c64b8b498e5c234a66877
Haruna Faruk <harunafaruk2004@gmail.com>
2023-10-09 19:51:31 +0100

    commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -32,7 +32,7 @@ router.route("/").post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error?.response.data.error.message, "Error Ended");
+    res.status(500).send(error?.response.data.error.message);
   }
 });
 
